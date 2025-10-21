99279d576293cb4557313a2397c92212a1383745
kryoshub421 <botelectrooo@gmail.com>
2025-10-22 01:57:15 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/7a1609eb4a257913669a0eea7c9ef651c6ce2e9f8d25ded5241ebcd0cfbc7f41/download"))()
+loadstring(game:HttpGet("https://pastefy.app/kGEsuKU4/raw"))()
